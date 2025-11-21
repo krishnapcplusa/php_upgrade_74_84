73f544c205e173df088bbe9cac27c0e3922f9ba7
Vibhore Jain <vibhore@pcplusa.com>
2025-11-21 16:22:28 +0530

    adding my sqli changes

--- sample-app/index.php
+++ sample-app/index.php
@@ -6,9 +6,27 @@
 // string offset access was removed.
 // ./lab use 74 to switch to 74
 // ./lab use 84 to switch to 84
-$str = "hello";
-$firstChar = $str{0}; // deprecated/removed syntax
 
-echo "<p>First char: $firstChar</p>";
+//Case 1:
+//$str = "hello";
+//$firstChar = $str{0}; // deprecated/removed syntax
+
+//echo "<p>First char: $firstChar</p>";
+
+
 
 phpinfo();
+//Case 2:
+$servername = "db";
+$username = "app";
+$password = "app";
+$dbname = "app";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully to MySQL!";
